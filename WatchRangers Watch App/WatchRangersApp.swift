d26850426fe8ffb5702eaffa1f5b3964fb41f1aa
Victor Castro <vhcastro@indracompany.com>
2023-01-30 08:28:41 -0500

    Initial Commit

---
+++ WatchRangers Watch App/WatchRangersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchRangersApp.swift
+//  WatchRangers Watch App
+//
+//  Created by Victor Castro on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchRangers_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchRangers/WatchRangersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchRangersApp.swift
+//  WatchRangers
+//
+//  Created by Victor Castro on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchRangersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
